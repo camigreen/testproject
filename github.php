af4d1f8082eb12a777d811ed9e4a3cb971c74184
Shawn Gibbons <sgibbons@palmettoimages.com>
2015-08-03 19:02:40 -0400

    sdf

--- github.php
+++ github.php
@@ -23,6 +23,7 @@ $BRANCH             = "master";
 echo 'start</br>';
 var_dump($_SERVER);
 var_dump($_POST);
+var_dump($_GET);
 if ( $_POST['payload'] ) {
 	echo 'yes';
   // Only respond to POST requests from Github
