317141dca8f531c031f946fb59a41efdfb1d38dd
Shawn Gibbons <sgibbons@palmettoimages.com>
2015-08-03 22:37:55 -0400

    sdf

--- github.php
+++ github.php
@@ -27,7 +27,7 @@
   // if( file_exists($LOCAL_REPO) ) {
 
     // If there is already a repo, just run a git pull to grab the latest changes
-    echo `git pull && ls`;
+    echo `ls`;
 
 //     die("done Repo Pulled" . time());
 //   } else {
